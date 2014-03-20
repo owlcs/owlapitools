e65f5e3641731b490f808fdf5acab620b879a723
ignazio <ignazio.palmisano@gmail.com>
2014-03-20 21:04:30 +0000

    update comparator to compile with v4

--- comparator/src/main/java/utils/reasonercomparator/OntologyLoader.java
+++ comparator/src/main/java/utils/reasonercomparator/OntologyLoader.java
@@ -11,11 +11,14 @@ import org.semanticweb.owlapi.util.AutoIRIMapper;
 
 /** utility to lad a number of ontologies */
 public class OntologyLoader {
+
     AutoIRIMapper mapper;
 
-    /** @param fileName
-     *            name
-     * @return ontology */
+    /**
+     * @param fileName
+     *        name
+     * @return ontology
+     */
     public OWLOntology getOntology(String fileName) {
         try {
             return OWLManager.createOWLOntologyManager()
@@ -33,38 +36,45 @@ public class OntologyLoader {
     private File baseFile;
     private File[] folders;
     final FilenameFilter filenameFilter = new FilenameFilter() {
+
         @Override
         public boolean accept(File arg0, String arg1) {
             return arg1.endsWith("_main.owl") || arg1.endsWith("_main.owl.zip");
         }
     };
 
-    /** @param baseFolder
-     *            base folder to list */
+    /**
+     * @param baseFolder
+     *        base folder to list
+     */
     public OntologyLoader(String baseFolder) {
         baseFile = new File(baseFolder);
         mapper = new AutoIRIMapper(baseFile, true);
         folders = baseFile.listFiles();
     }
 
-    /** @param folder
-     *            base folder
-     * @return ontology */
+    /**
+     * @param folder
+     *        base folder
+     * @return ontology
+     */
     public OWLOntology getOntology(File folder) {
         if (folder.isDirectory()) {
             File[] files = folder.listFiles(filenameFilter);
             if (files.length == 0) {
-                System.out.println("Atomize.getOntology() No main file for " + folder);
+                System.out.println("Atomize.getOntology() No main file for "
+                        + folder);
                 return null;
             }
             if (files.length > 1) {
-                System.out.println("Atomize.getOntology() More than one main file for "
-                        + folder + ", using: " + files[0].getName());
+                System.out
+                        .println("Atomize.getOntology() More than one main file for "
+                                + folder + ", using: " + files[0].getName());
             }
             File f = files[0];
             try {
                 OWLOntologyManager man = OWLManager.createOWLOntologyManager();
-                man.addIRIMapper(mapper);
+                man.getIRIMappers().add(mapper);
                 OWLOntology o = man.loadOntologyFromOntologyDocument(f);
                 return o;
             } catch (Throwable e) {
