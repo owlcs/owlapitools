1d78a615d536cbd51c8eeef63c83f2f2ae375add
ignazio <ignazio.palmisano@gmail.com>
2014-05-18 00:09:41 +0100

    allow config to be passed to comparator reasoners

--- comparator/src/main/java/utils/reasonercomparator/ComparisonExecutor.java
+++ comparator/src/main/java/utils/reasonercomparator/ComparisonExecutor.java
@@ -14,12 +14,16 @@ import org.semanticweb.owlapi.reasoner.InferenceType;
 import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.reasoner.NodeSet;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerConfiguration;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 
-/** Executes a set of reasoner calls
+/**
+ * Executes a set of reasoner calls
  * 
- * @author ignazio */
+ * @author ignazio
+ */
 public class ComparisonExecutor {
+
     private OWLReasonerFactory[] reasonerFactories;
     protected OWLOntology o1;
     protected OWLReasoner r;
@@ -30,10 +34,12 @@ public class ComparisonExecutor {
         return "stress test for " + name;
     }
 
-    /** run execution
+    /**
+     * run execution
      * 
      * @throws Exception
-     *             exception */
+     *         exception
+     */
     public void execute() throws Exception {
         r.getBottomClassNode();
         r.getBottomDataPropertyNode();
@@ -61,13 +67,15 @@ public class ComparisonExecutor {
         for (OWLClass c : classes) {
             r.isSatisfiable(c);
         }
-        Set<OWLObjectProperty> objectProperties = o1.getObjectPropertiesInSignature(true);
+        Set<OWLObjectProperty> objectProperties = o1
+                .getObjectPropertiesInSignature(true);
         Set<OWLDataProperty> dataProperties = o1.getDataPropertiesInSignature();
         Set<OWLNamedIndividual> individuals = o1.getIndividualsInSignature();
         for (OWLObjectProperty o : objectProperties) {
             checkObject(o);
         }
-        OWLClass thing = o1.getOWLOntologyManager().getOWLDataFactory().getOWLThing();
+        OWLClass thing = o1.getOWLOntologyManager().getOWLDataFactory()
+                .getOWLThing();
         {
             NodeSet<OWLClass> subclasses = r.getSubClasses(thing, false);
             NodeSet<OWLClass> superclasses = r.getSuperClasses(thing, false);
@@ -77,8 +85,10 @@ public class ComparisonExecutor {
                     for (Node<OWLClass> n1 : superclasses) {
                         for (OWLClass sup : n1) {
                             assertTrue(
-                                    "Wrong classification result! " + sub.getIRI()
-                                            + " not subclass of " + sup.getIRI()
+                                    "Wrong classification result! "
+                                            + sub.getIRI()
+                                            + " not subclass of "
+                                            + sup.getIRI()
                                             + " but it was supposed to be!",
                                     r.isEntailed(o1.getOWLOntologyManager()
                                             .getOWLDataFactory()
@@ -97,7 +107,8 @@ public class ComparisonExecutor {
                     for (Node<OWLClass> n1 : superclasses) {
                         for (OWLClass sup : n1) {
                             String message = "Wrong classification result! "
-                                    + sub.getIRI() + " not subclass of " + sup.getIRI()
+                                    + sub.getIRI() + " not subclass of "
+                                    + sup.getIRI()
                                     + " but it was supposed to be!";
                             assertTrue(
                                     message,
@@ -119,23 +130,26 @@ public class ComparisonExecutor {
         // r.dispose();
     }
 
-    /** check tautology
+    /**
+     * check tautology
      * 
      * @param index
-     *            repetition index */
+     *        repetition index
+     */
     public void checkTautology(int index) {
         for (OWLAxiom ax : o1.getLogicalAxioms()) {
             if (!r.isEntailed(ax)) {
                 System.out.println("ReloadingTestCallBack.checkTautology()");
                 r.isEntailed(ax);
             }
-            assertTrue("Axiom was supposed to be entailed! repetition: " + index
-                    + " axiom:" + ax, r.isEntailed(ax));
+            assertTrue("Axiom was supposed to be entailed! repetition: "
+                    + index + " axiom:" + ax, r.isEntailed(ax));
         }
     }
 
     private void checkIndividuals(Set<OWLObjectProperty> objectProperties,
-            Set<OWLDataProperty> dataProperties, Set<OWLNamedIndividual> individuals) {
+            Set<OWLDataProperty> dataProperties,
+            Set<OWLNamedIndividual> individuals) {
         for (OWLNamedIndividual i : individuals) {
             r.getTypes(i, false);
             r.getTypes(i, true);
@@ -186,17 +200,22 @@ public class ComparisonExecutor {
         r.getInverseObjectProperties(o);
     }
 
-    /** @param o
-     *            ontology
+    /**
+     * @param o
+     *        ontology
+     * @param c
+     *        configuration
      * @param f
-     *            reasoner factories
+     *        reasoner factories
      * @throws Exception
-     *             exception */
-    public ComparisonExecutor(OWLOntology o, OWLReasonerFactory... f) throws Exception {
+     *         exception
+     */
+    public ComparisonExecutor(OWLOntology o, OWLReasonerConfiguration c,
+            OWLReasonerFactory... f) throws Exception {
         o1 = o;
         name = "Comparing";
         reasonerFactories = f;
-        r = new ComparisonReasoner(o1, null, reasonerFactories);
+        r = new ComparisonReasoner(o1, c, reasonerFactories);
         r.precomputeInferences(InferenceType.values());
     }
 
--- comparator/src/main/java/utils/reasonercomparator/PerformanceComparator.java
+++ comparator/src/main/java/utils/reasonercomparator/PerformanceComparator.java
@@ -8,32 +8,32 @@ import java.util.Map;
 
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
-import org.semanticweb.owlapitools.profiles.OWL2DLProfile;
-import org.semanticweb.owlapitools.profiles.OWLProfileReport;
-import org.semanticweb.owlapitools.profiles.OWLProfileViolation;
 
 /** The Class PerformanceComparator. */
 public class PerformanceComparator {
-    /** Run test.
+
+    /**
+     * Run test.
      * 
      * @param o1
-     *            the o1
+     *        the o1
      * @param factories
-     *            the factories
+     *        the factories
      * @return the list
      * @throws Exception
-     *             the exception */
+     *         the exception
+     */
     public static List<ReasonerPerformanceResult> runTest(OWLOntology o1,
             OWLReasonerFactory... factories) throws Exception {
         Checker checker = new Checker(1);
-        OWL2DLProfile profile = new OWL2DLProfile();
-        OWLProfileReport report = profile.checkOntology(o1);
-        for (OWLProfileViolation<?> v : report.getViolations()) {
-            System.out.println(v);
-        }
+        // OWL2DLProfile profile = new OWL2DLProfile();
+        // OWLProfileReport report = profile.checkOntology(o1);
+        // for (OWLProfileViolation<?> v : report.getViolations()) {
+        // System.out.println(v);
+        // }
         List<ReasonerPerformanceResult> toReturn = new ArrayList<ReasonerPerformanceResult>();
         // TODO number of runs
-        ComparisonExecutor ccb = new ComparisonExecutor(o1, factories);
+        ComparisonExecutor ccb = new ComparisonExecutor(o1, null, factories);
         checker.check(ccb);
         String trace = checker.getTrace();
         System.out.println(trace);
@@ -43,31 +43,36 @@ public class PerformanceComparator {
         return toReturn;
     }
 
-    /** Run.
+    /**
+     * Run.
      * 
      * @param ontologies
-     *            the ontologies
+     *        the ontologies
      * @param factories
-     *            the factories
+     *        the factories
      * @return the string
      * @throws Exception
-     *             the exception */
+     *         the exception
+     */
     public static String run(Collection<OWLOntology> ontologies,
             OWLReasonerFactory... factories) throws Exception {
         Map<String, List<ReasonerPerformanceResult>> list = new HashMap<String, List<ReasonerPerformanceResult>>();
         for (OWLOntology o : ontologies) {
-            list.put(o.getOntologyID().getOntologyIRI().toString(), runTest(o, factories));
+            list.put(o.getOntologyID().getOntologyIRI().toString(),
+                    runTest(o, factories));
         }
         return toString(list);
     }
 
-    /** To string.
+    /**
+     * To string.
      * 
      * @param timingsList
-     *            the timings list
-     * @return the string */
-    public static String
-            toString(Map<String, List<ReasonerPerformanceResult>> timingsList) {
+     *        the timings list
+     * @return the string
+     */
+    public static String toString(
+            Map<String, List<ReasonerPerformanceResult>> timingsList) {
         StringBuilder b = new StringBuilder("method,");
         b.append("calls,");
         for (MethodNames m : MethodNames.values()) {
