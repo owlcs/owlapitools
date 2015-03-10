eaf6e5edbe6b98a9abdede756de7e444c3859158
Dmitry Tsarkov <dmitry.tsarkov@gmail.com>
2015-03-10 22:11:24 +0000

    slightly improve locality testcases

--- atomicdecomposition/src/test/java/decomposition/test/SemanticLocalityTestCase.java
+++ atomicdecomposition/src/test/java/decomposition/test/SemanticLocalityTestCase.java
@@ -12,24 +12,10 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLAnnotationProperty;
-import org.semanticweb.owlapi.model.OWLAxiom;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLDataProperty;
-import org.semanticweb.owlapi.model.OWLDatatype;
-import org.semanticweb.owlapi.model.OWLDisjointUnionAxiom;
-import org.semanticweb.owlapi.model.OWLEntity;
-import org.semanticweb.owlapi.model.OWLLiteral;
-import org.semanticweb.owlapi.model.OWLNamedIndividual;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
-import org.semanticweb.owlapi.model.SWRLAtom;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
 
-import decomposition.AxiomWrapper;
-import decomposition.SemanticLocalityChecker;
+import decomposition.*;
 
 @Ignore
 @SuppressWarnings("javadoc")
@@ -545,19 +531,18 @@ public class SemanticLocalityTestCase {
 
     @Before
     public void setUp() {
-        testSubject = new SemanticLocalityChecker(null,
-                OWLManager.createOWLOntologyManager());
+    	OWLReasonerFactory factory = null;
+    //	OWLReasonerFactory factory = new FaCTPlusPlusReasonerFactory();
+    //	OWLReasonerFactory factory = new JFactFactory();
+        testSubject = new SemanticLocalityChecker(factory, OWLManager.createOWLOntologyManager());
     }
 
     private void set(OWLEntity... entities) {
-        testSubject.getSignature().addAll(Arrays.asList(entities));
+        testSubject.setSignatureValue(new Signature(Arrays.asList(entities)));
     }
 
     private void test(OWLAxiom ax, boolean expected, OWLEntity... entities) {
-        testSubject.preprocessOntology(Arrays.asList(new AxiomWrapper(ax)));
-        set(entities);
-        boolean local = testSubject.local(ax);
-        assertEquals(expected, local);
+    	test(ax, expected, false, entities);
     }
 
     private void test(OWLAxiom ax, boolean expected, boolean locality,
